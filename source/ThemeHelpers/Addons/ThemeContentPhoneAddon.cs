e64d9c234f652f5fb876130ebdd00187511a9bf0
jkidwell <jay@contensive.com>
2021-04-14 10:43:56 -0400

    Added Theme-Content-Phone

---
+++ source/ThemeHelpers/Addons/ThemeContentPhoneAddon.cs
@@ -0,0 +1,20 @@
+﻿
+using Contensive.Addons.ThemeHelpers.Controllers;
+
+namespace Contensive.Addons.ThemeHelpers.Addons {
+    /// <summary>
+    /// Template Footer hours of operation
+    /// </summary>
+    public class ThemeContentPhoneAddon : Contensive.BaseClasses.AddonBaseClass {
+        // 
+        // ====================================================================================================
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CP"></param>
+        /// <returns></returns>
+        public override object Execute(BaseClasses.CPBaseClass CP) {
+            return GenericController.getLayout(CP, "Theme-Content-Phone", "{F3CF29BC-9E94-4DFB-9B47-0866B635D570}", "themehelpers\\Theme-Content-Phone.html");
+        }
+    }
+}
